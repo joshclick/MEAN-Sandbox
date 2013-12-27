ad49e2aa35d7a4c933db478744509b7920bde74c
Josh Click <joshuaqian8@gmail.com>
2013-12-27 01:15:20 -0500

    split up core.js further

---
+++ public/js/controllers/main.js
@@ -0,0 +1,32 @@
+angular.module('todoController', [])
+    
+    // inject the Todo service factory into our controller
+    .controller('mainController', function($scope, $http, Todos) {
+        $scope.formData = {};
+
+        // when landing on the page, get all todos and show them
+        Todos.get()
+            .success(function(data) {
+                $scope.todos = data;
+            });
+
+        // when submitting the add form, send the text to the node API
+        $scope.createTodo = function() {
+            if (!$.isEmptyObject($scope.formData)) {
+                Todos.create($scope.formData)
+                    .success(function(data) {
+                        $scope.formData = {}; // clear form
+                        $scope.todos = data;
+                    });
+            }
+        };
+
+        // delete a todo after checking it
+        $scope.deleteTodo = function(id) {
+            Todos.delete(id)
+                .success(function(data) {
+                    $scope.todos = data;
+                });
+        };
+
+    });
---
+++ public/js/core.js
@@ -0,0 +1,1 @@
+angular.module('jcTodo', ['todoController', 'todoService']);
